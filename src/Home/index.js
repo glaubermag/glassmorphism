7367e6f34bac161ce0aa5d27cfe6ee687e3bab5e
Glauber Ariel Magalhães <glaubermagalhaes@outlook.com>
2021-01-26 21:16:19 -0300

    upload images and create main files

---
+++ src/Home/index.js
@@ -0,0 +1,13 @@
+// import react from 'react'
+import { Container } from './styles';
+
+function Home() {
+  return (
+<Container> 
+<header className="App-header">
+<h1> teste </h1>
+      </header>
+      </Container>  );
+}
+
+export default Home;
